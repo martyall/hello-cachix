e1ad4b47b87c6ddc8124b4cafe8c90541723aabd
martyall <martyall@protonmail.com>
2022-12-09 20:27:02 +0000

    switch to haskell.nix

--- flake.nix
+++ flake.nix
@@ -1,56 +1,94 @@
-# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
-#
-# SPDX-License-Identifier: CC0-1.0
-
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-  
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
-  };
-
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
-
-  let
-    supportedSystems = [
-      "x86_64-linux"
-    ];
-  in
-    flake-utils.lib.eachSystem supportedSystems (system:
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+      ];
+    in
+      flake-utils.lib.eachSystem supportedSystems (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays =
+          [ haskellNix.overlay
+              (final: prev: {
+                hixProject =
+                  final.haskell-nix.project {
+                    src = ./.;
+                    compiler-nix-name = "ghc902";
+                    evalSystem = "x86_64-linux";
+                  };
+                }
+              )
+          ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.hixProject.flake {};
+      in flake // rec
+           { packages =  
+               { foo = flake.packages."foo:exe:foo";
+                 bar = flake.packages."bar:exe:bar";
+                 all = pkgs.symlinkJoin {
+                   name = "all";
+                   paths = with packages;
+                     [ foo
+                       bar
+                     ];
+                 };
+                 default = packages.all;
+               };
+           }
+      );
 
-        haskellPackages = pkgs.haskell.packages.ghc90.override {
-           overrides = self: super: rec {
-             foo = self.callCabal2nix "foo" (./foo) {};
-           };
-        };
-
-
-      in rec {
-        packages =
-          { foo = haskellPackages.callCabal2nix "foo" ./foo {};
-            bar = haskellPackages.callCabal2nix "bar" ./bar {};
-            all = pkgs.symlinkJoin {
-              name = "all";
-              paths = with packages;
-                [ foo 
-                  bar
-                ];
-              };
-            default = packages.all;
-          };
-         devShells.default = pkgs.mkShell {
-           buildInputs = with pkgs; [
-             ghcid
-             cabal-install
-             ormolu
-           ];
-           inputsFrom = builtins.attrValues self.packages.${system};
-         };
-      });
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = true;
+  };
 }
+#      });
+#  nixConfig = {
+#    # This sets the flake to use the IOG nix cache.
+#    # Nix should ask for permission before using it,
+#    # but remove it here if you do not want it to.
+#    extra-substituters = ["https://cache.iog.io"];
+#    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+#    allow-import-from-derivation = "true";
+#  };
+#      in flake // rec {
+#        legacyPackages = pkgs;
+#        devShells =
+#          { default =
+#             pkgs.hixProject.shellFor {
+#               tools = {
+#                 cabal = "3.6.0.0"; # this is the version specified in all of the *.cabal files.
+#                 haskell-language-server = "latest";
+#               };
+#             };
+#          };
+#        packages =
+#          { djed-pab-operator = flake.packages."djed-pab:exe:djed-pab-operator";
+#            djed-pab-user = flake.packages."djed-pab:exe:djed-pab-user";
+#            djed-pab-host = flake.packages."djed-pab:exe:djed-pab-host";
+#            djed-chain-index = flake.packages."djed-pab:exe:djed-chain-index";
+#            djed-oracle-operator = flake.packages."djed-client:exe:djed-oracle-operator";
+#            djed-stablecoin-operator = flake.packages."djed-client:exe:djed-stablecoin-operator";
+#            djed-stablecoin-client = flake.packages."djed-client:exe:djed-stablecoin-client";
+#            djed-client-test = flake.packages."djed-client:test:djed-client-test";
+#            all = pkgs.symlinkJoin {
+#              name = "all";
+#              paths = with packages;
+#                [ djed-pab-operator
+#                  djed-pab-user
+#                  djed-pab-host
+#                  djed-chain-index
+#                  djed-oracle-operator
+#                  djed-stablecoin-operator
+#                  djed-stablecoin-client
+#                ];
+#            };
+#            default = packages.all;
+#          };
